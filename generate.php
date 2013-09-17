20992a09084f70756837b79c338cbdd61f47d37c
Tommi Airikka <tommi@airikka.net>
2013-09-17 20:59:23 +0200

    Adds first changes of config file feature

--- generate.php
+++ generate.php
@@ -1,6 +1,3 @@
-<html>
-<title>Simpel Burndown Graph Generator</title>
-<body>
 <?php
 
 $days = $_POST['days'];
@@ -8,11 +5,17 @@ $date = $_POST['date'];
 $hours = $_POST['hours'];
 $we = $_POST['we'];
 $title = $_POST['title'];
+$graph_button = isset($_POST['graph_button']);
+$configfile = $_FILES['file']['tmp_name'];
 
-header("Content-type:application/pdf");
+if($graph_button){
+header("Content-type: application/pdf");
 header("Content-Disposition: attachment; filename=burndown-".date('Y-m-d-h-i-s').".pdf");
-passthru("./generate_graph --output stdout --start-date $date --work-days $days --hours $hours --with-hd-we $we --title $title");
+passthru("./generate --output stdout-graph --start-date $date --work-days $days --hours $hours --with-hd-we $we --title $title --config $configfile");
+} else {
+header("Content-type: text/plain");
+header("Content-Disposition: attachment; filename=burndown-config".date('Y-m-d-h-i-s').".txt");
+passthru("./generate --output stdout-config --start-date $date --work-days $days --hours $hours --with-hd-we $we --title $title --config $configfile");
+}
 
 ?>
-<body>
-</html>
--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <html>
 <title>Simpel Burndown Graph Generator</title>
 <body>
-<h2>Simple Burndown Graph Generator</h2><br>
-<form action="generate.php" method="post">
+<h2>Simpel Burndown Graph Generator</h2><br>
+<form action="generate.php" method="post" enctype="multipart/form-data">
 Title: <input type="text" name="title"><br>
 Work days: <input type="text" name="days"><br>
 Start date: <input type="text" name="date"><br>
 Hours: <input type="text" name="hours"><br>
-Show weekends/holidays: <input type="radio" name="we" value="yes" checked>Yes <input type="radio" name="we" value="no">No<br>
-<input type="submit">
+Show weekends/holidays: <input type="radio" name="we" value="yes" checked>Yes <input type="radio" name="we" value="no">No<br><br>
+Config file:<br>
+<input type="file" name="file" id="file"><br><br>
+<input type="submit" name="graph_button" value="Generate graph">
+<input type="submit" name="config_button" value="Generate config file">
 </form>
 <body>
 </html>
