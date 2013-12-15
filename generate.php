2d2cc48391d366eaf7044b785d3fb3e186cb8838
Tommi Airikka <tommi.airikka@hiq.se>
2013-12-15 13:31:21 +0100

    Fix that makes it possible to allow # sign in title

--- generate.php
+++ generate.php
@@ -11,11 +11,11 @@ $configfile = $_FILES['file']['tmp_name'];
 if($graph_button){
 header("Content-type: application/pdf");
 header("Content-Disposition: attachment; filename=burndown-".date('Y-m-d-h-i-s').".pdf");
-passthru("./generate --output stdout-graph --start-date $date --work-days $days --hours $hours --with-hd-we $we --title $title --config $configfile");
+passthru("./generate --output stdout-graph --start-date $date --work-days $days --hours $hours --with-hd-we $we --title \"$title\" --config $configfile");
 } else {
 header("Content-type: text/plain");
 header("Content-Disposition: attachment; filename=burndown-config-".date('Y-m-d-h-i-s').".txt");
-passthru("./generate --output stdout-config --start-date $date --work-days $days --hours $hours --with-hd-we $we --title $title --config $configfile");
+passthru("./generate --output stdout-config --start-date $date --work-days $days --hours $hours --with-hd-we $we --title \"$title\" --config $configfile");
 }
 
 ?>
