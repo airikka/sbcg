d132a75d3ae4b85eceff5e997885947ca71d966d
Tommi Airikka <tommi.airikka@hiq.se>
2014-01-28 17:48:39 +0100

    Rename from graph to chart

--- generate.php
+++ generate.php
@@ -5,13 +5,13 @@ $date = $_POST['date'];
 $hours = $_POST['hours'];
 $we = $_POST['we'];
 $title = $_POST['title'];
-$graph_button = isset($_POST['graph_button']);
+$chart_button = isset($_POST['chart_button']);
 $configfile = $_FILES['file']['tmp_name'];
 
-if($graph_button){
+if($chart_button){
 header("Content-type: application/pdf");
 header("Content-Disposition: attachment; filename=burndown-".date('Y-m-d-h-i-s').".pdf");
-passthru("./generate --output stdout-graph --start-date $date --work-days $days --hours $hours --with-hd-we $we --title \"$title\" --config $configfile");
+passthru("./generate --output stdout-chart --start-date $date --work-days $days --hours $hours --with-hd-we $we --title \"$title\" --config $configfile");
 } else {
 header("Content-type: text/plain");
 header("Content-Disposition: attachment; filename=burndown-config-".date('Y-m-d-h-i-s').".txt");
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
-<title>Simple Burndown Graph Generator</title>
+<title>Simple Burndown Chart Generator</title>
 <body>
-<h2>Simple Burndown Graph Generator</h2><br>
+<h2>Simple Burndown Chart Generator</h2><br>
 <form action="generate.php" method="post" enctype="multipart/form-data">
 Title: <input type="text" name="title"><br>
 Work days:
@@ -36,23 +36,23 @@ for ($i=1; $i<=1000; $i++)
 Show weekends/holidays: <input type="radio" name="we" value="yes">Yes <input type="radio" name="we" value="no">No<br><br>
 Config file:<br>
 <input type="file" name="file" id="file"><br><br>
-<input type="submit" name="graph_button" value="Generate graph">
+<input type="submit" name="chart_button" value="Generate chart">
 <input type="submit" name="config_button" value="Generate config file">
 </form>
 <br><br><br>
 <code>
 Simple howto:<br><br>
-There are two ways of using Simple Burndown Graph Generator.<br><br>
-1. If just a simple burndown graph with "Ideal" line is needed, just enter values and push "Generate graph" button<br><br>
+There are two ways of using Simple Burndown Chart Generator.<br><br>
+1. If just a simple burndown chart with "Ideal" line is needed, just enter values and push "Generate chart" button<br><br>
 2. If an "Actual" line is needed:<br>
 <ul>
 <li> Enter starting values
 <li> Push "Generate config file"
 <li> Edit config file with "Actual" values
-<li> Each time you want to generate a new graph, just select your config file and push "Generate graph" button
+<li> Each time you want to generate a new chart, just select your config file and push "Generate chart" button
 </ul>
 <br><br>
-Get the source code <a href="https://github.com/airikka/sbgg">here</a>
+Get the source code <a href="https://github.com/airikka/sbcg">here</a>
 </code>
 </body>
 </html>
